26d9fe7e84f7a0a6bd1be7ede636c4b67519029f
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-19 17:38:02 +0300

    increase button done

--- script.js
+++ script.js
@@ -59,5 +59,21 @@ function drawLine (x1, y1, x2, y2) {
     ctx.stroke()
 }
 
+function updateSizeOnScreen() {
+    sizeEl.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if (size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
 colorEl.addEventListener('change', (e) => color = e.target.value
 )
+
+
