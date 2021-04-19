e828dbc1a02411f6db49cca19916a0b0075dac90
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-19 17:40:59 +0300

    clear button done

--- script.js
+++ script.js
@@ -83,7 +83,9 @@ decreaseBtn.addEventListener('click', () => {
     updateSizeOnScreen()
 })
 
+
+
 colorEl.addEventListener('change', (e) => color = e.target.value
 )
 
-
+clearEl.addEventListener('click', () => ctx.clearRect(0,0,canvas.width,canvas.height))
