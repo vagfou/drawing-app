5b6e56180036c395ec1ea84d919aaa9ed1704da5
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-19 17:39:27 +0300

    decrease button done

--- script.js
+++ script.js
@@ -73,6 +73,16 @@ increaseBtn.addEventListener('click', () => {
     updateSizeOnScreen()
 })
 
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if (size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
 colorEl.addEventListener('change', (e) => color = e.target.value
 )
 
